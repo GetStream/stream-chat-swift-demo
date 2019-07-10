2f49f78e3bc66dc88f3a5b35011cda613b622944
Alexey Bukhtin <bukhtin@gmail.com>
2019-07-10 14:11:00 +0200

    Custom Theme.

--- ChatDemo/DemoChatViewController.swift
+++ ChatDemo/DemoChatViewController.swift
@@ -10,25 +10,22 @@ import UIKit
 import StreamChat
 
 class DemoChatViewController: ChatViewController {
-    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        // Setup the general channel for the chat.
+        
+        // Set the general channel.
         let channel = Channel(id: "general", name: "General")
         channelPresenter = ChannelPresenter(channel: channel)
-    }
-    
-    /// Override the default implementation of UI messages
-    /// with default UIKit table view cell.
-    override func messageCell(at indexPath: IndexPath, message: Message,    readUsers: [User]) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "message")
-            ?? UITableViewCell(style: .value2, reuseIdentifier: "message")
         
-        cell.textLabel?.text = message.user.name
-        cell.textLabel?.font = .systemFont(ofSize: 12, weight: .bold)
-        cell.detailTextLabel?.text = message.text
-        cell.detailTextLabel?.numberOfLines = 0
+        // Update styles.
+        style.incomingMessage.chatBackgroundColor = UIColor(hue: 0.2, saturation: 0.3, brightness: 1, alpha: 1)
+        style.incomingMessage.backgroundColor = UIColor(hue: 0.6, saturation: 0.5, brightness: 1, alpha: 1)
+        style.incomingMessage.borderWidth = 0
         
-        return cell
+        style.outgoingMessage.chatBackgroundColor = style.incomingMessage.chatBackgroundColor
+        style.outgoingMessage.backgroundColor = style.incomingMessage.chatBackgroundColor
+        style.outgoingMessage.font = .systemFont(ofSize: 15, weight: .bold)
+        style.outgoingMessage.cornerRadius = 0
+        style.outgoingMessage.showCurrentUserAvatar = false
     }
 }
