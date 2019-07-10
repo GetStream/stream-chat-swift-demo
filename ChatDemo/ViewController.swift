3769d1bca22e9b017dc525ce5eb47655137c6531
Alexey Bukhtin <bukhtin@gmail.com>
2019-07-10 13:55:12 +0200

    ChatDemo project created.

---
+++ ChatDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatDemo
+//
+//  Created by Stream on 10/07/2019.
+//  Copyright © 2019 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
