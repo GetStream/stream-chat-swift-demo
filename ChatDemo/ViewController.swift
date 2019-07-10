f15677cb9752ed9361afe2589335453d2abfdb04
Alexey Bukhtin <bukhtin@gmail.com>
2019-07-10 14:08:06 +0200

    Customize Channel Preview.

--- ChatDemo/ViewController.swift
+++ ChatDemo/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+// Add Stream Chat SDK.
+import StreamChat
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+/// Use ChannelsViewController as the parent view controller.
+class ViewController: ChannelsViewController {
+    /// We override the inherited method for a channel cell.
+    /// Here we can create absolutely new table view cell for the channel.
+    override func channelCell(at indexPath: IndexPath, channelPresenter: ChannelPresenter) -> UITableViewCell {
+        // For now, get the default channel cell.
+        let cell = super.channelCell(at: indexPath, channelPresenter: channelPresenter)
+        
+        // We need to check if the returned cell is ChannelTableViewCell.
+        guard let channelCell = cell as? ChannelTableViewCell else {
+            return cell
+        }
+        
+        // Check the number of unread messages.
+        if channelPresenter.unreadCount > 0 {
+            // Add the info about unread messages to the cell.
+            channelCell.update(info: "\(channelPresenter.unreadCount) unread", bold: true)
+        }
+        
+        return channelCell
     }
-
-
 }
-
