6ef73f3e6925b133f4f50a3c16e38184d976f651
cardoso <matheus@cardo.so>
2020-05-26 22:55:53 -0300

    Remote unneeded function

--- ChatDemo/AppDelegate.swift
+++ ChatDemo/AppDelegate.swift
@@ -16,10 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        return true
-    }
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
