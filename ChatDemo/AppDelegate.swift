58ee7b32644239339cc303794fa87757417ff3d2
Matheus Cardoso <matheus@cardo.so>
2020-06-06 06:12:36 -0300

    Update to Stream Chat 2.2 (#7)
    * Update Stream Chat version
    
    * Update code
    
    * Remote unneeded function

--- ChatDemo/AppDelegate.swift
+++ ChatDemo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import StreamChatClient
 import StreamChatCore
 import StreamChat
 
@@ -16,16 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Client.config = .init(apiKey: "b67pax5b2wdq")
-        
-        Client.shared.set(user: User(id: "proud-mountain-3", name: "Proud mountain"),
-                          token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoicHJvdWQtbW91bnRhaW4tMyJ9.HIxKKB1IiOlFTpI8Xax5gLiP_V4IlaZpt4-1vfYF-_U")
-        
-        if let navigationController = window?.rootViewController as? UINavigationController,
-            let channelsViewController = navigationController.viewControllers.first as? ChannelsViewController {
-            channelsViewController.channelsPresenter = ChannelsPresenter(channelType: .messaging, filter: .key("members", .in(["proud-mountain-3"])))
-        }
-        
         return true
     }
 }
--- Podfile
+++ Podfile
@@ -3,5 +3,5 @@ inhibit_all_warnings!
 
 target 'ChatDemo' do
   use_frameworks!
-  pod 'StreamChat'
+  pod 'StreamChat', '~> 2.2'
 end
