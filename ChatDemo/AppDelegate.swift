c2825a75cc1bd108aea4905260406391f816aa25
Matheus Cardoso <matheus@cardo.so>
2020-06-17 11:59:58 -0300

    Update to iOS 13 scene delegate lifecycle (#8)
    * Update to iOS 13 session delegate lifecycle
    
    * Make code more like the tutorial

--- ChatDemo/AppDelegate.swift
+++ ChatDemo/AppDelegate.swift
@@ -14,9 +14,28 @@ import StreamChat
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Client.configureShared(.init(apiKey: "m7cdet3kpfdu", logOptions: .info))
+        
+        let userExtraData = UserExtraData(name: "Square art")
+        Client.shared.set(user: User(id: "square-art-0", extraData: userExtraData),
+                          token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoic3F1YXJlLWFydC0wIn0.HDTQ8dDHSndescdgmi-T6Ta0yORvVD6sYLXB08CqZaM")
         return true
     }
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
 }
